85769fa4c3e9b533bf57ec43010fb3161c11d178
Chris Allen <christopherallen777@gmail.com>
2017-06-19 09:21:43 -0500

    First push to Github

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'BxVh3G6IHIuoFJjQcRmh6nAJr',
+  consumer_secret: 'FTltXmPSN2zncNxw86r5yUTXtwNepI5QCx4XQQg7t8mYiTE1S1',
+  access_token_key: '1107657140-6wPTGV972Rs5EC21g8M7qfyiZAQHV0EcPQ4Qkn7',
+  access_token_secret: 'LK6JJcsJRnyTY9L4FzRUu7VBfzSnERvIboe24w4sYFLNS',
+}
+
