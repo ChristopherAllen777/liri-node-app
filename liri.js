4c819596c855fda207af11447644cd99c01d7f8f
Chris Allen <christopherallen777@gmail.com>
2017-06-19 15:43:22 -0500

    Working : Movie, Twitter, Spotify. Needs to be cleaned up for proper return results.

--- liri.js
+++ liri.js
@@ -1,6 +1,76 @@
 var keys = require("./keys.js");
 var request = require("request");
+var request = require("request");
+var Twitter = require('twitter');
 var inquirer = require("inquirer");
+var Spotify = require('node-spotify-api');
+var fs = require("fs");
+ 
+// Twitter API 
+var client = new Twitter({
+  consumer_key: 'BxVh3G6IHIuoFJjQcRmh6nAJr',
+  consumer_secret: 'FTltXmPSN2zncNxw86r5yUTXtwNepI5QCx4XQQg7t8mYiTE1S1',
+  access_token_key: '1107657140-6wPTGV972Rs5EC21g8M7qfyiZAQHV0EcPQ4Qkn7',
+  access_token_secret: 'LK6JJcsJRnyTY9L4FzRUu7VBfzSnERvIboe24w4sYFLNS'
+});
+ 
+var params = {screen_name: 'chrisallen777'};
+client.get('search/tweets', {q: 'node.js', limit: 1}, function(error, tweets, response) {
+	console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log("Twitter API Tweet Search Results Below");
+    console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log("");
+   console.log(tweets);
+});
+// Twitter API
+
+
+
+
+// // Spotify API 
+var spotify = new Spotify({
+  id: '54d72841e6a940adb2b5bf919507c6f6',
+  secret: '1181e12735de4805b84ecf1705dd0352'
+});
+ 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+
+spotify
+  .search({ type: 'track', query: 'All the Small Things', limit: 1 })
+  .then(function(response) {
+  	console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log("Spotify Track Search Results Below");
+    console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log("");
+    console.log(response);
+    console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log("");
+  })
+  .catch(function(err) {
+  	console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log(error);
+    console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log("");
+});
+// // Spotify API
+
+// console.log("");
+// console.log("-------------------------------------");
+// console.log("This is our Spotify data: " + data);
+// console.log("-------------------------------------");
+// console.log("");
+
+
 
 // Store all of the arguments in an array
 var nodeArgs = process.argv;
@@ -40,6 +110,7 @@ request(queryUrl, function(error, response, body) {
     // Then log the body from the site!
     console.log("");
     console.log("-------------------------------------");
+    console.log("-------------------------------------");
     console.log("The Title of the movie: " + JSON.parse(body).Title);
     console.log("-------------------------------------");
     console.log("The release year of the movie: " + JSON.parse(body).Year);
@@ -53,6 +124,7 @@ request(queryUrl, function(error, response, body) {
     console.log("Genre: " + JSON.parse(body).Genre);
     console.log("Actors: " + JSON.parse(body).Actors);
     console.log("Awards: " + JSON.parse(body).Awards);
+    console.log("-------------------------------------");
     console.log("-------------------------------------");
     console.log("");
 
